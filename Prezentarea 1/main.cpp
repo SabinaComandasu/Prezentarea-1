f4f89b8c31f89cba749f1a46a7dafb803145c9ee
SabinaComandasu <comandasu.sabina.x9g@student.ucv.ro>
2026-03-21 21:11:12 +0200

    Renamed project and solution to Blossom Haven

--- Prezentarea 1/main.cpp
+++ Prezentarea 1/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv)
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(1200, 800);
-    glutCreateWindow("P1 - Scena intr-un cub");
+    glutCreateWindow("Blossom Haven");
 
     init();
 
